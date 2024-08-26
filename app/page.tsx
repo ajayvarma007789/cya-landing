34772b961d51d985bdd8b3fb729f89b374cca6b0
Abin Karukappallil <followgamer102@gmail.com>
2024-08-26 17:09:49 +0530

    Add: card to display vision and mission

--- app/page.tsx
+++ app/page.tsx
@@ -426,7 +426,7 @@ export default function Home() {
           
         </div>
       </section>
-      <section className="flex flex-col w-full items-center p-7 gap-7 max-h-screen overflow-clip">
+      <section className="flex flex-col w-full items-center p-7 m-2 gap-7 max-h-screen overflow-clip">
         <h1 className={cn("text-neutral-300 text-center font-bold text-4xl md:text-6xl z-10", fearFont.className)}>ABOUT</h1>
         <h1 className={cn("text-neutral-300 text-center font-bold text-lg my-2 md:text-3xl  z-10", fearFont.className)}>DEPARTMENT</h1>
         <CardHoverEffectDemo/>
